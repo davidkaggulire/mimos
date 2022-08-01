a0e430512a2162b8ed4968a410b2927b0ea9c799
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-02 01:29:38 +0300

    urls for given resources

---
+++ utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:8000";
+
+export const getAllUsersRoute = `${host}/api/v1/users`;
+export const registerRoute = `${host}/api/v1/users/signup`;
+export const loginRoute = `${host}/api/v1/users/login`;
+
+export const sendMessageRoute = `${host}/api/v1/messages/addMessage`;
+export const getAllMessageRoute = `${host}/api/v1/messages/getMessages`;
