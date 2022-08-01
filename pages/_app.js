b162c7007353fb90f755da6a97f5003fb1cd9bd0
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-02 01:25:30 +0300

    include toastify in project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import "react-toastify/dist/ReactToastify.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+
+  // return (
+  //   <>
+  //     <Provider store={store}>
+  //       <Component {...pageProps} />
+  //     </Provider>
+  //   </>
+  // );
 }
 
-export default MyApp
+export default MyApp;
