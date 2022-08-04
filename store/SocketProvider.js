41c96cae0d8ca593f17a8f774b376b34cf91eaea
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-04 17:15:18 +0300

    add react context provider for socket

---
+++ store/SocketProvider.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useRef, useState } from "react";
+import io from "socket.io-client";
+import { host } from "../utils/APIRoutes";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+const SocketProvider = ({ children }) => {
+  const socket = useRef();
+  socket.current = io(host, {
+    withCredentials: true,
+  });
+
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
+
+export default SocketProvider;
