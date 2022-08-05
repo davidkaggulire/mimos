f736906effec2b17a9a17a8e3b885c1e80dbc838
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-05 13:36:53 +0300

    enable production

--- utils/APIRoutes.js
+++ utils/APIRoutes.js
@@ -1,9 +1,10 @@
-export const host = "http://localhost:8000";
-// export const host = "https://mimose.herokuapp.com"
+// export const host = "http://localhost:8000";
+export const host = "https://mimose.herokuapp.com"
 
 export const getAllUsersRoute = `${host}/api/v1/users`;
 export const registerRoute = `${host}/api/v1/users/signup`;
 export const loginRoute = `${host}/api/v1/users/login`;
+export const forgotPassword = `${host}/api/v1/users/forgotPassword`;
 
 export const sendMessageRoute = `${host}/api/v1/messages/addMessage`;
 export const getAllMessageRoute = `${host}/api/v1/messages/getMessages`;
