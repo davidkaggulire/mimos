e9155cb9bd81f580c88142b392493acc1130e0de
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-09 20:37:50 +0300

    render current user image

--- components/Users/Contacts.js
+++ components/Users/Contacts.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { Fragment, useEffect, useState } from "react";
+import Modal from "../UI/Modal";
 import classes from "./Contacts.module.css";
+import Profile from "./Profile";
 
 function Contacts(props) {
   const { contacts, currentUser, changeChat } = props;
   const [currentUserName, setCurrentUserName] = useState(undefined);
   const [currentUserImage, setCurrentUserImage] = useState(undefined);
   const [currentSelected, setCurrentSelected] = useState(undefined);
+
+  const [imageFormShow, setImageFormShow] = useState(false);
 
   useEffect(() => {
     if (currentUser) {
@@ -21,33 +25,55 @@ function Contacts(props) {
     changeChat(contact);
   };
 
+  const showImageFormHandler = () => {
+    setImageFormShow(true);
+  };
+
+  const hideImageFormHandler = () => {
+    setImageFormShow(false);
+  };
+
   return (
     <Fragment>
-      <div className={classes.contacts}>
-        <h2 className={classes.app__heading}>Mimos</h2>
-        {contacts.map((contact, index) => {
-          return (
-            <div
-            className={`${classes.user__details} ${
-              index === currentSelected ? "selected" : ""
-            }`}
+      {imageFormShow && <Profile onClose={hideImageFormHandler} />}
 
-              // className={classes.user__details}
-              key={index}
-              onClick={() => changeCurrentChat(index, contact)}
-            >
-              <Image
-                className={classes.avatar}
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=400"
-                alt="avatar"
-                height={50}
-                width={50}
-              />
-              <h4>{contact.username}</h4>
-            </div>
-          );
-        })}
-      </div>
+      {currentUserImage && (
+        <div className={classes.contacts}>
+          <div className={classes.app__user}>
+            <h2 className={classes.app__heading}>Mimos</h2>
+            <Image
+              className={classes.avatar}
+              src={currentUserImage}
+              alt="avatar"
+              height={50}
+              width={50}
+              onClick={showImageFormHandler}
+            />
+          </div>
+
+          {contacts.map((contact, index) => {
+            return (
+              <div
+                className={`${classes.user__details} ${
+                  index === currentSelected ? "selected" : ""
+                }`}
+                // className={classes.user__details}
+                key={index}
+                onClick={() => changeCurrentChat(index, contact)}
+              >
+                <Image
+                  className={classes.avatar}
+                  src={contact.photo}
+                  alt="avatar"
+                  height={50}
+                  width={50}
+                />
+                <h4>{contact.username}</h4>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </Fragment>
   );
 }
