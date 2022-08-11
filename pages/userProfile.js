e25fbd0571d66096516ef78b6dc77a5029e46368
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-11 17:54:20 +0300

    create UserProfile page

---
+++ pages/userProfile.js
@@ -0,0 +1,9 @@
+import classes from './../components/Users/UserProfile.module.css';
+
+function UserProfile() {
+    return <div className={classes.hello}>
+        Hello
+    </div>
+}
+
+export default UserProfile;
