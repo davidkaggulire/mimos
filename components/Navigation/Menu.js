4bfd1babf034926b031b82b011fe69766d625814
davidkaggulire1 <dkaggulire@gmail.com>
2022-08-12 22:50:35 +0300

    include logout option on menu

--- components/Navigation/Menu.js
+++ components/Navigation/Menu.js
@@ -3,10 +3,18 @@ import Link from "next/link";
 import Sidebar from "../UI/Sidebar";
 import classes from "./Menu.module.css";
 import { MdSettings } from "react-icons/md";
+import { BiPowerOff } from "react-icons/bi";
+import { useRouter } from "next/router";
 
 function Menu(props) {
   const { currentUser } = props;
+  const router = useRouter();
   console.log(currentUser);
+
+  const handleLogout = async () => {
+    localStorage.clear();
+    router.push("/login");
+  };
 
   return (
     <Sidebar onClose={props.onClose}>
@@ -24,13 +32,19 @@ function Menu(props) {
           <h3>{currentUser.username}</h3>
         </div>
         <ul className={classes.side__element}>
-          <li onClick={props.onClose} className={classes.side__link}>
+          <li className={classes.side__link}>
             <Link href="/userProfile">
               <div className={classes.actions}>
                 <MdSettings className={classes.side__icon} />
                 Settings
               </div>
             </Link>
+          </li>
+          <li className={classes.side__link} onClick={handleLogout}>
+            <div className={classes.actions} onClick={handleLogout}>
+              <BiPowerOff className={classes.logout__icon} />
+              Logout
+            </div>
           </li>
         </ul>
       </nav>
